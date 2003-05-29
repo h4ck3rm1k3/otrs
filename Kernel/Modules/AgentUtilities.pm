7a6288e677972c98addfc7ae37129d21bbe17f8c
martin <martin>
2003-05-29 16:04:35 +0000

    added shown tickt owner name

---
+++ Kernel/Modules/AgentUtilities.pm
@@ -0,0 +1,414 @@
+# --
+# Kernel/Modules/AgentUtilities.pm - Utilities for tickets
+# Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
+# --
+# $Id: AgentUtilities.pm,v 1.25.2.1 2003/05/29 16:09:19 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+
+package Kernel::Modules::AgentUtilities;
+
+use strict;
+use Kernel::System::CustomerUser;
+
+use vars qw($VERSION);
+$VERSION = '$Revision: 1.25.2.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
+
+# --
+sub new {
+    my $Type = shift;
+    my %Param = @_;
+
+    # allocate new hash for object    
+    my $Self = {}; 
+    bless ($Self, $Type);
+    
+    foreach (keys %Param) {
+        $Self->{$_} = $Param{$_};
+    }
+
+    # check needed Opjects
+    foreach (qw(ParamObject DBObject TicketObject LayoutObject LogObject ConfigObject)) {
+        die "Got no $_!" if (!$Self->{$_});
+    }
+    $Self->{CustomerUserObject} = Kernel::System::CustomerUser->new(%Param);
+
+    # get params
+    $Self->{Want} = $Self->{ParamObject}->GetParam(Param => 'Want') || '';
+    $Self->{StartHit} = $Self->{ParamObject}->GetParam(Param => 'StartHit') || 1;
+    # get confid data
+    $Self->{SearchLimit} = $Self->{ConfigObject}->Get('SearchLimit') || 200;
+    $Self->{SearchPageShown} = $Self->{ConfigObject}->Get('SearchPageShown') || 15;
+    return $Self;
+}
+# --
+sub Run {
+    my $Self = shift;
+    my %Param = @_;
+    my $Output;
+    
+    if ($Self->{Subaction} eq 'Search') {
+        $Output = $Self->Search();
+    }
+    else {
+        $Output = $Self->Form();
+    }
+    return $Output;
+}
+# --
+sub Form {
+    my $Self = shift;
+    my %Param = @_;
+    my $Output;
+    my $UserID = $Self->{UserID};
+
+    # --
+    # get user of own groups
+    # --
+    my %ShownUsers = ();
+    my %AllGroupsMembers = $Self->{UserObject}->UserList(
+        Type => 'Long',
+        Valid => 1,
+    );
+    if ($Self->{ConfigObject}->Get('ChangeOwnerToEveryone')) {
+        %ShownUsers = %AllGroupsMembers;
+    }
+    else {
+        my %Groups = $Self->{GroupObject}->GroupUserList(
+            UserID => $Self->{UserID},
+            Type => 'rw',
+            Result => 'HASH',
+        );
+        foreach (keys %Groups) {
+            my %MemberList = $Self->{GroupObject}->GroupMemberList(
+                GroupID => $_,
+                Type => 'rw',
+                Result => 'HASH',
+            );
+            foreach (keys %MemberList) {
+                $ShownUsers{$_} = $AllGroupsMembers{$_};
+            }
+        }
+    }
+    
+    $Output .= $Self->{LayoutObject}->Header(Title => 'Utilities');
+    my %LockedData = $Self->{TicketObject}->GetLockedCount(UserID => $UserID);
+    $Output .= $Self->{LayoutObject}->NavigationBar(LockData => \%LockedData);
+    $Output .= $Self->{LayoutObject}->AgentUtilForm(
+        Users => \%ShownUsers,
+    );
+    $Output .= $Self->{LayoutObject}->Footer();
+    
+    return $Output;
+}
+# --
+sub Search {
+    my $Self = shift;
+    my %Param = @_;
+    my $Output;
+    my $Want = $Self->{Want};
+    my $UserID = $Self->{UserID};
+    my @WhatFields = $Self->{ParamObject}->GetArray(Param => 'What');
+    my @States = $Self->{ParamObject}->GetArray(Param => 'State');
+    my @QueueIDs = $Self->{ParamObject}->GetArray(Param => 'QueueID');
+    my @PriorityIDs = $Self->{ParamObject}->GetArray(Param => 'PriorityID');
+    my @UserIDs = $Self->{ParamObject}->GetArray(Param => 'UserID');
+    my $TicketNumber = $Self->{ParamObject}->GetParam(Param => 'TicketNumber') || '';
+    $Output .= $Self->{LayoutObject}->Header(Title => 'Utilities');
+    my %LockedData = $Self->{TicketObject}->GetLockedCount(UserID => $UserID);
+    $Output .= $Self->{LayoutObject}->NavigationBar(LockData => \%LockedData);
+    # --
+    # add states to where statement
+    # --
+    my $SqlTicketNumberExt = '';
+    if ($TicketNumber) {
+        # modifier search string!!!
+        my $TicketNumberTmp = $TicketNumber;
+        $TicketNumberTmp =~ s/\*/%/g;
+        $SqlTicketNumberExt .= " st.tn LIKE '$TicketNumberTmp' ";
+    }
+    my $SqlUserExt = '';
+    foreach (@UserIDs) {
+        $SqlUserExt = " st.user_id IN ( ${\(join ', ', @UserIDs)} )";
+    }
+    my $SqlStateExt = '';
+    my $CounterTmp = 0;
+    foreach (@States) {
+        if ($CounterTmp != 0) {
+            $SqlStateExt .= " or ";
+        }
+        $CounterTmp++;
+        if (my $StateID = $Self->{TicketObject}->StateLookup(State => $_)) {
+            $SqlStateExt .= " st.ticket_state_id = $StateID ";
+        }
+    }
+    my $SqlQueueExt = '';
+    $CounterTmp = 0;
+    foreach (@QueueIDs) {
+        if ($CounterTmp != 0) {
+            $SqlQueueExt .= " or ";
+        }
+        $CounterTmp++;
+        $SqlQueueExt .= " sq.id = $_ ";
+    }
+    my $SqlPriorityExt = '';
+    $CounterTmp = 0;
+    foreach (@PriorityIDs) {
+        if ($CounterTmp != 0) {
+            $SqlPriorityExt .= " or ";
+        }
+        $CounterTmp++;
+        $SqlPriorityExt .= " st.ticket_priority_id = $_ ";
+    }
+    # --
+    # modifier search string
+    # --
+    $Want =~ s/\+/%/gi;
+    $Want =~ s/\*//gi;
+    my @SParts = split('%', $Want);
+    # --
+    # show search again table
+    # --
+    # --
+    # get user of own groups
+    # --
+    my %ShownUsers = ();
+    my %AllGroupsMembers = $Self->{UserObject}->UserList(
+        Type => 'Long',
+        Valid => 1,
+    );
+    if ($Self->{ConfigObject}->Get('ChangeOwnerToEveryone')) {
+        %ShownUsers = %AllGroupsMembers;
+    }
+    else {
+        my %Groups = $Self->{GroupObject}->GroupUserList(
+            UserID => $Self->{UserID},
+            Type => 'rw',
+            Result => 'HASH',
+        );
+        foreach (keys %Groups) {
+            my %MemberList = $Self->{GroupObject}->GroupMemberList(
+                GroupID => $_,
+                Type => 'rw',
+                Result => 'HASH',
+            ); 
+            foreach (keys %MemberList) {
+                $ShownUsers{$_} = $AllGroupsMembers{$_};
+            }
+        }
+    }
+    $Output .= $Self->{LayoutObject}->AgentUtilSearchAgain(
+        TicketNumber => $TicketNumber,
+        What => $Want,
+        Kind => 'SearchByText',
+        Limit => $Self->{SearchLimit},
+        WhatFields => \@WhatFields,
+        SelectedStates => \@States,
+        SelectedQueueIDs => \@QueueIDs,
+        SelectedPriorityIDs => \@PriorityIDs,
+        Users => \%ShownUsers,
+        SelectedUserIDs => \@UserIDs,
+    );
+
+    # --
+    # build a spez. sql ext.
+    # --
+    my $SqlExt = '';
+    my %FieldSQLMap = (
+        From => 'sa.a_from',
+        To => 'sa.a_to',
+        Cc => 'sa.a_cc',
+        Subject => 'sa.a_subject',
+        Body => 'sa.a_body',
+        CustomerID => 'st.customer_id',
+        TicketFreeText => 'st.freetext',
+        ArticleFreeText => 'sa.a_freetext',
+    );
+    my @SearchFields = ();
+    foreach my $Field (@WhatFields) {
+        if ($FieldSQLMap{$Field}) {
+          if ($Field eq 'TicketFreeText') {
+            foreach (1..2) {
+              push (@SearchFields, "$FieldSQLMap{$Field}$_");
+            }
+          }
+          elsif ($Field eq 'ArticleFreeText') {
+            foreach (1..3) {
+              push (@SearchFields, "$FieldSQLMap{$Field}$_");
+            }
+          }
+          else {
+              push (@SearchFields, $FieldSQLMap{$Field});
+          }
+        }
+    }
+    # --
+    # create tricky sql part
+    # --
+    $CounterTmp = 0;
+    if (@SParts) {
+        foreach my $Field (@SearchFields) {
+            if ($CounterTmp != 0) {
+                $SqlExt .= " or ";
+            }
+            $CounterTmp++;
+            my $CounterTmp1 = 0;
+            foreach (@SParts) {
+                if ($CounterTmp1 != 0) {
+                    $SqlExt .= " and ";
+                }
+                $CounterTmp1++;
+                $SqlExt .= " $Field LIKE '%$_%' ";
+            }
+        }
+    }
+    # --
+    # if there is no search field selected!
+    # --
+    if (!@SearchFields) {
+        # --
+        # error page
+        # --
+        $Output = $Self->{LayoutObject}->Header(Title => 'Error');
+        $Output .= $Self->{LayoutObject}->Error(
+            Message => "Can't search! Select min. one search field!",
+            Comment => 'Please Select min. one search field.',
+        );
+        $Output .= $Self->{LayoutObject}->Footer();
+        return $Output;
+    }
+    # --
+    # get users groups
+    # --
+    my @GroupIDs = $Self->{GroupObject}->GroupUserList(
+        UserID => $Self->{UserID},
+        Type => 'ro',
+        Result => 'ID',
+    );
+    # --
+    # db query
+    # --
+    my $OutputTables = '';
+    my $Age = '?';
+    my $SQL = "";
+    if ($SqlExt) {
+        $SQL = "SELECT sa.id ".
+          " FROM ".
+          " article sa, ticket st, queue sq ".
+          " WHERE ".
+          " sa.ticket_id = st.id ".
+          " AND ";
+    }
+    else {
+        $SQL = "SELECT st.id ".
+          " FROM ".
+          " ticket st, queue sq ".
+          " WHERE ";
+    }
+    $SQL .= " sq.id = st.queue_id ".
+      " AND ".
+      " sq.group_id IN ( ${\(join ', ', @GroupIDs)} )";
+    if ($SqlTicketNumberExt) {
+        $SQL .= " AND $SqlTicketNumberExt";
+    }
+    if ($SqlUserExt) {
+        $SQL .= " AND $SqlUserExt";
+    } 
+    if ($SqlQueueExt) { 
+        $SQL .= " AND ($SqlQueueExt)"; 
+    }
+    if ($SqlStateExt) {
+        $SQL .= " AND ($SqlStateExt)";
+    }
+    if ($SqlPriorityExt) {
+        $SQL .= " AND ($SqlPriorityExt)";
+    }
+    if ($SqlExt) {
+        $SQL .= " AND ($SqlExt) ";
+    }
+    $SQL .= ""; 
+    $SQL .= " ORDER BY st.id DESC";
+#    $SQL .= " GROUP BY st.id, sa.id ORDER BY st.id DESC";
+#    " ORDER BY sa.incoming_time DESC";
+
+    $Self->{DBObject}->Prepare(SQL => $SQL, Limit => $Self->{SearchLimit});
+    my @ViewableIDs = ();
+    my $Counter = 0;
+    while (my @Row = $Self->{DBObject}->FetchrowArray() ) {
+         push (@ViewableIDs, $Row[0]);
+    }
+    foreach (@ViewableIDs) {
+      $Counter++;
+      # --
+      # build search result
+      # --
+      if ($Counter >= $Self->{StartHit} && $Counter < ($Self->{SearchPageShown}+$Self->{StartHit}) ) {
+        my %Data = ();
+        my %Article = ();
+        if ($SqlExt) {
+            %Article = $Self->{TicketObject}->GetArticle(ArticleID => $_);
+            %Data = $Self->{TicketObject}->GetTicket(TicketID => $Article{TicketID});
+        }
+        else {
+            %Article = $Self->{TicketObject}->GetLastCustomerArticle(TicketID => $_);
+            %Data = $Self->{TicketObject}->GetTicket(TicketID => $Article{TicketID});
+        }
+        # --
+        # customer info
+        # --
+        my %CustomerData = ();
+        if ($Article{CustomerUserID}) {
+            %CustomerData = $Self->{CustomerUserObject}->CustomerUserDataGet(
+                User => $Article{CustomerUserID},
+            );
+        }
+        elsif ($Article{CustomerID}) {
+            %CustomerData = $Self->{CustomerUserObject}->CustomerUserDataGet(
+                CustomerID => $Article{CustomerID},
+            );
+        }
+        $OutputTables .= $Self->{LayoutObject}->AgentUtilSearchResult(
+            %Data,
+            ArticleID => $_,
+            From => $Article{From},
+            To => $Article{To},
+            Subject => $Article{Subject},
+            Body => $Article{Body},
+            Age => $Article{Age},
+            Priority => $Article{Priority},
+            Queue => $Article{Queue},
+            ContentCharset => $Article{ContentCharset},
+            MimeType => $Article{MimeType},
+            What => $Want,
+            Highlight => 1,
+            CustomerData => \%CustomerData,
+        );
+      } 
+    }
+    # --
+    # build search navigation bar
+    # --
+    my $SearchNavBar = $Self->{LayoutObject}->AgentUtilSearchCouter(
+        TicketNumber => $TicketNumber,
+        Limit => $Self->{SearchLimit}, 
+        StartHit => $Self->{StartHit}, 
+        SearchPageShown => $Self->{SearchPageShown},
+        AllHits => $Counter,
+        WhatFields => \@WhatFields,
+        Want => $Self->{Want},
+        SelectedStates => \@States,
+        SelectedQueueIDs => \@QueueIDs,
+        SelectedPriorityIDs => \@PriorityIDs,
+        SelectedUserIDs => \@UserIDs,
+    );
+    $Output .= $SearchNavBar.$OutputTables;
+    $Output .= $Self->{LayoutObject}->Footer();
+    return $Output;
+}
+# --
+
+1;
--- Kernel/Modules/AgentZoom.pm
+++ Kernel/Modules/AgentZoom.pm
@@ -1,63 +1,294 @@
 # --
 # Kernel/Modules/AgentZoom.pm - to get a closer view
-# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: AgentZoom.pm,v 1.98 2011/12/19 09:03:35 mg Exp $
+# $Id: AgentZoom.pm,v 1.34.2.1 2003/05/29 16:04:35 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 # --
-
-#
-# LEGACY: This module redirects to AgentTicketZoom. It should be kept for a while
-#   because existing legacy/upgraded systems have it in their notifications.
-#   To drop it, existing notifications would have to be changed by the database
-#   upgrading script.
-#
 
 package Kernel::Modules::AgentZoom;
 
 use strict;
-use warnings;
-
 use Kernel::System::CustomerUser;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.98 $) [1];
+$VERSION = '$Revision: 1.34.2.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
+# --
 sub new {
-    my ( $Type, %Param ) = @_;
-
-    # allocate new hash for object
-    my $Self = {%Param};
-    bless( $Self, $Type );
-
-    # check needed objects
-    for (qw(ParamObject DBObject LayoutObject LogObject ConfigObject )) {
-        if ( !$Self->{$_} ) {
-            $Self->{LayoutObject}->FatalError( Message => "Got no $_!" );
-        }
+    my $Type = shift;
+    my %Param = @_;
+   
+    # allocate new hash for object 
+    my $Self = {}; 
+    bless ($Self, $Type);
+    
+    foreach (keys %Param) {
+        $Self->{$_} = $Param{$_};
     }
 
+    # check needed Opjects
+    foreach (qw(ParamObject DBObject TicketObject LayoutObject LogObject
+      QueueObject ConfigObject UserObject SessionObject)) {
+        die "Got no $_!" if (!$Self->{$_});
+    }
+
+    # get ArticleID
+    $Self->{ArticleID} = $Self->{ParamObject}->GetParam(Param => 'ArticleID');
+
+    # customer user object
+    $Self->{CustomerUserObject} = Kernel::System::CustomerUser->new(%Param);
+    
     return $Self;
 }
-
+# --
 sub Run {
-    my ( $Self, %Param ) = @_;
-
-    # compat link
-    my $Redirect = $ENV{REQUEST_URI};
-    if ($Redirect) {
-        $Redirect =~ s/AgentZoom/AgentTicketZoom/;
+    my $Self = shift;
+    my %Param = @_;
+    my $Output;
+    my $QueueID = $Self->{TicketObject}->GetQueueIDOfTicketID(TicketID => $Self->{TicketID});
+    # --
+    # check needed stuff
+    # --
+    if (!$Self->{TicketID} || !$QueueID) {
+        $Output = $Self->{LayoutObject}->Header(Title => 'Error');
+        $Output .= $Self->{LayoutObject}->Error();
+        $Output .= $Self->{LayoutObject}->Footer();
+        return $Output;
+    }
+    # --
+    # check permissions
+    # --
+    if (!$Self->{TicketObject}->Permission(
+        Type => 'ro',
+        TicketID => $Self->{TicketID},
+        UserID => $Self->{UserID})) {
+        # --
+        # error screen, don't show ticket
+        # --
+        return $Self->{LayoutObject}->NoPermission(WithHeader => 'yes');
+    }  
+    # --
+    # store last screen
+    # --
+    if ($Self->{Subaction} ne 'ShowHTMLeMail') {
+      if (!$Self->{SessionObject}->UpdateSessionID(
+        SessionID => $Self->{SessionID},
+        Key => 'LastScreen',
+        Value => $Self->{RequestedURL},
+      )) {
+        $Output = $Self->{LayoutObject}->Header(Title => 'Error');
+        $Output .= $Self->{LayoutObject}->Error();
+        $Output .= $Self->{LayoutObject}->Footer();
+        return $Output;
+      }  
+    }
+    # --
+    # fetch all queues
+    # --
+    my %MoveQueues = ();
+    if ($Self->{ConfigObject}->Get('MoveInToAllQueues')) {
+        %MoveQueues = $Self->{QueueObject}->GetAllQueues();
     }
     else {
-        $Redirect
-            = $Self->{LayoutObject}->{Baselink}
-            . 'Action=AgentTicketZoom;TicketID='
-            . $Self->{TicketID};
+        %MoveQueues = $Self->{QueueObject}->GetAllQueues(
+            UserID => $Self->{UserID},
+            Type => 'rw',
+        );
     }
-    return $Self->{LayoutObject}->Redirect( OP => $Redirect );
+    # --
+    # fetch all std. responses
+    # --
+    my %StdResponses = $Self->{QueueObject}->GetStdResponses(QueueID => $QueueID);
+    
+    my %Ticket;
+    $Ticket{TicketID} = $Self->{TicketID};
+    $Ticket{Age} = '?';
+    $Ticket{TmpCounter} = 0;
+    $Ticket{FreeKey1} = '';
+    $Ticket{FreeValue1} = '';
+    $Ticket{FreeKey2} = '';
+    $Ticket{FreeValue2} = '';
+    $Ticket{TicketTimeUnits} = $Self->{TicketObject}->GetAccountedTime(TicketID => $Ticket{TicketID});
+    # --
+    # grep all atricle of this ticket
+    # --
+    my @ArticleBox;
+    my $SQL = "SELECT sa.id, st.tn, sa.a_from, sa.a_to, sa.a_cc, sa.a_subject, sa.a_body, ".
+    " st.create_time_unix, st.tn, st.user_id, st.ticket_state_id, st.ticket_priority_id, ". 
+    " sa.create_time, stt.name as sender_type, at.name as article_type, ".
+    " su.$Self->{ConfigObject}->{DatabaseUserTableUser}, ".
+    " sl.name as lock_type, sp.name as priority, sp.id as priority_id, tsd.name as state, sa.content_path, ".
+    " sq.name as queue, st.create_time as ticket_create_time, ".
+    " sa.a_freekey1, sa.a_freetext1, sa.a_freekey2, sa.a_freetext2, ".
+    " sa.a_freekey3, sa.a_freetext3, st.freekey1, st.freekey2, st.freetext1, ".
+    " st.freetext2, st.customer_id, st.customer_user_id, sq.group_id, st.ticket_answered, ".
+    " sq.escalation_time, sa.a_content_type, sa.incoming_time, st.until_time ".
+    " FROM ".
+    " article sa, ticket st, article_sender_type stt, article_type at, ".
+    " $Self->{ConfigObject}->{DatabaseUserTable} su, ticket_lock_type sl, " .
+    " ticket_priority sp, ticket_state tsd, queue sq " .
+    " WHERE " .
+    " sa.ticket_id = st.id " .
+    " AND " .
+    " sq.id = st.queue_id " .
+    " AND " .
+    " stt.id = sa.article_sender_type_id " .
+    " AND " .
+    " at.id = sa.article_type_id " .
+    " AND " .
+    " sp.id = st.ticket_priority_id " .
+    " AND " .
+    " sl.id = st.ticket_lock_id " .
+    " AND " .
+    " tsd.id = st.ticket_state_id " .
+    " AND " .
+    " sa.ticket_id = $Self->{TicketID} " .
+    " AND " .
+    " su.$Self->{ConfigObject}->{DatabaseUserTableUserID} = st.user_id " .
+    " GROUP BY sa.id, st.tn, sa.a_from, sa.a_to, sa.a_cc, sa.a_subject, sa.a_body, ".
+    " st.create_time_unix, st.tn, st.user_id, st.ticket_state_id, st.ticket_priority_id, ".
+    " sa.create_time, stt.name, at.name, ".
+    " su.$Self->{ConfigObject}->{DatabaseUserTableUser}, ".
+    " sl.name, sp.name, sp.id, tsd.name, sa.content_path, ".
+    " sq.name, st.create_time, ".
+    " sa.a_freekey1, sa.a_freetext1, sa.a_freekey2, sa.a_freetext2, ".
+    " sa.a_freekey3, sa.a_freetext3, st.freekey1, st.freekey2, st.freetext1, ".
+    " st.freetext2, st.customer_id, st.customer_user_id, sq.group_id, st.ticket_answered, ".
+    " sq.escalation_time, sa.a_content_type, sa.incoming_time, st.until_time ";
+    $Self->{DBObject}->Prepare(SQL => $SQL);
+    while (my $Data = $Self->{DBObject}->FetchrowHashref() ) {
+        # get escalation_time
+        if ($$Data{escalation_time} && $$Data{sender_type} eq 'customer') {
+            $Ticket{TicketOverTime} = (($$Data{incoming_time} + ($$Data{escalation_time}*60)) - time());
+        }
+        if (!$$Data{until_time} || $$Data{state} !~ /^pending/i) {
+            $Ticket{UntilTime} = 0;
+        }
+        else {
+            $Ticket{UntilTime} = $$Data{until_time} - time();
+        }
+        # ticket data
+        $Ticket{TicketNumber} = $$Data{tn};
+        $Ticket{State} = $$Data{state};
+        $Ticket{CustomerID} = $$Data{customer_id};
+        $Ticket{CustomerUserID} = $$Data{customer_user_id};
+        $Ticket{Queue} = $$Data{queue};
+        $Ticket{QueueID} = $QueueID;
+        $Ticket{Lock} = $$Data{lock_type};
+        $Ticket{Owner} = $$Data{login};
+        $Ticket{Priority} = $$Data{priority};
+        $Ticket{PriorityID} = $$Data{priority_id};
+        $Ticket{TicketFreeKey1} = $$Data{freekey1};
+        $Ticket{TicketFreeValue1} = $$Data{freetext1};
+        $Ticket{TicketFreeKey2} = $$Data{freekey2};
+        $Ticket{TicketFreeValue2} = $$Data{freetext2};
+        $Ticket{Created} = $$Data{ticket_create_time};
+        $Ticket{GroupID} = $$Data{group_id};
+        $Ticket{Age} = time() - $$Data{create_time_unix};
+        $Ticket{Answered} = $$Data{ticket_answered};
+        # article data
+        my %Article;
+        $Article{ContentPath} = $$Data{content_path},
+        $Article{ArticleType} = $$Data{article_type};
+        $Article{SenderType} = $$Data{sender_type};
+        $Article{ArticleID} = $$Data{id};
+        $Article{From} = $$Data{a_from} || '';
+        $Article{To} = $$Data{a_to} || '';
+        $Article{Cc} = $$Data{a_cc} || '';
+        $Article{Subject} = $$Data{a_subject} || '';
+        $Article{Body} = $$Data{a_body};
+        $Article{CreateTime} = $$Data{create_time};
+        $Article{FreeKey1} = $$Data{a_freekey1};
+        $Article{FreeValue1} = $$Data{a_freetext1};
+        $Article{FreeKey2} = $$Data{a_freekey2};
+        $Article{FreeValue2} = $$Data{a_freetext2};
+        $Article{FreeKey3} = $$Data{a_freekey3};
+        $Article{FreeValue3} = $$Data{a_freetext3};
+        if ($$Data{a_content_type} && $$Data{a_content_type} =~ /charset=(.*)(| |\n)/i) {
+            $Article{ContentCharset} = $1;
+        }
+        if ($$Data{a_content_type} && $$Data{a_content_type} =~ /^(.+?\/.+?)( |;)/i) {
+            $Article{MimeType} = $1;
+        }
+        push (@ArticleBox, \%Article);
+    }
+    # --
+    # article attachments
+    # --
+    foreach my $Article (@ArticleBox) {
+        my %AtmIndex = $Self->{TicketObject}->GetArticleAtmIndex(
+            ContentPath => $Article->{ContentPath},
+            ArticleID => $Article->{ArticleID},
+        );
+        $Article->{Atms} = \%AtmIndex;
+    }
+    # --
+    # user info
+    # --
+    my %UserInfo = $Self->{UserObject}->GetUserData(
+        User => $Ticket{Owner},
+        Cached => 1
+    );
+    # --
+    # customer info
+    # --
+    my %CustomerData = ();
+    if ($Self->{ConfigObject}->Get('ShowCustomerInfoZoom')) {
+        if ($Ticket{CustomerUserID}) {
+            %CustomerData = $Self->{CustomerUserObject}->CustomerUserDataGet(
+                User => $Ticket{CustomerUserID},
+            );
+        }
+        elsif ($Ticket{CustomerID}) {
+            %CustomerData = $Self->{CustomerUserObject}->CustomerUserDataGet(
+                CustomerID => $Ticket{CustomerID},
+            );
+        }
+    }
+    # --
+    # genterate output
+    # --
+    $Output .= $Self->{LayoutObject}->Header(Title => "Zoom Ticket");
+    my %LockedData = $Self->{TicketObject}->GetLockedCount(UserID => $Self->{UserID});
+    $Output .= $Self->{LayoutObject}->NavigationBar(LockData => \%LockedData);
+    # --
+    # show ticket
+    # --
+    if ($Self->{Subaction} eq 'ShowHTMLeMail') {
+        # if it is a html email, drop normal header
+        $Ticket{ShowHTMLeMail} = 1;
+        $Output = '';
+    }
+    $Output .= $Self->{LayoutObject}->AgentZoom(
+        TicketID => $Self->{TicketID},
+        QueueID => $QueueID,
+        MoveQueues => \%MoveQueues,
+        StdResponses => \%StdResponses,
+        ArticleBox => \@ArticleBox,
+        ArticleID => $Self->{ArticleID},
+        %Ticket,
+        %UserInfo,
+        CustomerData => \%CustomerData,
+    );
+    # --
+    # return if HTML email
+    # --
+    if ($Self->{Subaction} eq 'ShowHTMLeMail') {
+        # if it is a html email, return here
+        $Ticket{ShowHTMLeMail} = 1;
+        return $Output;
+    }
+    # add footer 
+    $Output .= $Self->{LayoutObject}->Footer();
+
+    # return output
+    return $Output;
 }
+# --
 
 1;
