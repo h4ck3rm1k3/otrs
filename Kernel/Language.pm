399b66307487e49bd1648f42150543789b113891
martin <martin>
2003-08-07 06:17:45 +0000

    fixed FormatTimeString() - time format

--- Kernel/Language.pm
+++ Kernel/Language.pm
@@ -2,7 +2,7 @@
 # Kernel/Language.pm - provides multi language support
 # Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: Language.pm,v 1.18.2.1 2003/05/21 13:45:52 martin Exp $
+# $Id: Language.pm,v 1.18.2.2 2003/08/07 06:17:45 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 
 use vars qw(@ISA $VERSION);
 
-$VERSION = '$Revision: 1.18.2.1 $';
+$VERSION = '$Revision: 1.18.2.2 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 # --
@@ -189,7 +189,7 @@ sub FormatTimeString {
     my $Self = shift;
     my $String = shift || return;
     my $ReturnString = $Self->{DateFormat} || 'DateFormat needs to be translated!';
-    if ($String =~ /^(\d\d\d\d)-(\d\d)-(\d\d)\s(\d\d:\d\d:\d\d)$/) {
+    if ($String =~ /^(\d\d\d\d)-(\d\d)-(\d\d)\s(\d\d:\d\d:\d\d)/) {
         my ($Y,$M,$D, $T) = ($1, $2, $3, $4);
         $ReturnString =~ s/\%T/$T/g;
         $ReturnString =~ s/\%D/$D/g;
