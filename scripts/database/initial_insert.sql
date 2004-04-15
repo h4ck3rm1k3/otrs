7495c50e03cc03473614a91811501633f55faac0
martin <martin>
2004-04-15 08:43:14 +0000

    removed not needed ticket_history_types

--- scripts/database/initial_insert.sql
+++ scripts/database/initial_insert.sql
@@ -2,9 +2,12 @@
 -- initial_insert.sql - provides initial system data
 -- Copyright (C) 2001-2003 Martin Edenhofer <martin+code@otrs.org>
 -- --
--- $Id: initial_insert.sql,v 1.23 2004/04/02 12:49:35 martin Exp $
+-- $Id: initial_insert.sql,v 1.24 2004/04/15 08:43:14 martin Exp $
 -- 
 -- $Log: initial_insert.sql,v $
+-- Revision 1.24  2004/04/15 08:43:14  martin
+-- removed not needed ticket_history_types
+--
 -- Revision 1.23  2004/04/02 12:49:35  martin
 -- added missing WebRequestCustomer ticket_history_type entry
 --
@@ -477,23 +480,7 @@ INSERT INTO ticket_history_type
 INSERT INTO ticket_history_type
         (name, valid_id, create_by, create_time, change_by, change_time)
         VALUES
-        ('Close successful', 1, 1, current_timestamp, 1, current_timestamp);
-INSERT INTO ticket_history_type
-        (name, valid_id, create_by, create_time, change_by, change_time)
-        VALUES
-        ('Close unsuccessful', 1, 1, current_timestamp, 1, current_timestamp);
-INSERT INTO ticket_history_type
-        (name, valid_id, create_by, create_time, change_by, change_time)
-        VALUES
         ('AddNote', 1, 1, current_timestamp, 1, current_timestamp);
-INSERT INTO ticket_history_type
-        (name, valid_id, create_by, create_time, change_by, change_time)
-        VALUES
-        ('Open', 1, 1, current_timestamp, 1, current_timestamp);
-INSERT INTO ticket_history_type
-        (name, valid_id, create_by, create_time, change_by, change_time)
-        VALUES
-        ('Reopen', 1, 1, current_timestamp, 1, current_timestamp);
 INSERT INTO ticket_history_type
         (name, valid_id, create_by, create_time, change_by, change_time)
         VALUES
@@ -538,10 +525,6 @@ INSERT INTO ticket_history_type
         (name, valid_id, create_by, create_time, change_by, change_time)
         VALUES
         ('SetPendingTime', 1, 1, current_timestamp, 1, current_timestamp);
-INSERT INTO ticket_history_type
-        (name, valid_id, create_by, create_time, change_by, change_time)
-        VALUES
-        ('SetPending', 1, 1, current_timestamp, 1, current_timestamp);
 INSERT INTO ticket_history_type
         (name, valid_id, create_by, create_time, change_by, change_time)
         VALUES
