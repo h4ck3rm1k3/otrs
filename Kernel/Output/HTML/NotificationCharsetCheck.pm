c388768bc4e9bdac5daeb5f7f9a8ffc82dbfb97c
martin <martin>
2004-04-15 08:40:21 +0000

    removed not needed ticket obejcts

--- Kernel/Output/HTML/NotificationCharsetCheck.pm
+++ Kernel/Output/HTML/NotificationCharsetCheck.pm
@@ -1,15 +1,15 @@
 # --
-# Kernel/Output/HTML/NotificationUIDCheck.pm  
+# Kernel/Output/HTML/NotificationCharsetCheck.pm  
 # Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: NotificationUIDCheck.pm,v 1.1.2.1 2004/04/15 08:40:21 martin Exp $
+# $Id: NotificationCharsetCheck.pm,v 1.1.2.1 2004/04/15 08:40:21 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 # --
 
-package Kernel::Output::HTML::NotificationUIDCheck;
+package Kernel::Output::HTML::NotificationCharsetCheck;
 
 use strict;
 
@@ -30,6 +30,7 @@ sub new {
     foreach (qw(ConfigObject LogObject DBObject LayoutObject UserID)) {
         $Self->{$_} = $Param{$_} || die "Got no $_!";
     }
+
     return $Self;
 }
 # --
@@ -37,10 +38,20 @@ sub Run {
     my $Self = shift;
     my %Param = @_;
     my $Output = '';
-    if ($Self->{UserID} == 1) {
+    # check DisplayCharset
+    if ($Self->{LayoutObject}->{UserCharset} =~ /^utf-8$/i) {
+        $Param{CorrectDisplayCharset} = 1;
+    }
+    else {
+        foreach ($Self->{LayoutObject}->{LanguageObject}->GetPossibleCharsets()) {
+            if ($Self->{LayoutObject}->{UserCharset} =~ /^$_$/i) {
+                $Param{CorrectDisplayCharset} = 1;
+            }
+        }
+    }
+    if (!$Param{CorrectDisplayCharset} && $Self->{LayoutObject}->{LanguageObject}->GetRecommendedCharset()) {
         $Output .= $Self->{LayoutObject}->Notify(
-          Info => '<a href="$Env{"Baselink"}Action=AdminUser">'.
-          $Self->{LayoutObject}->{LanguageObject}->Get("Don't work with UserID 1 (System account)! Create new users!").'</a>',
+          Info => $Self->{LayoutObject}->{LanguageObject}->Get('The recommended charset for your language is %s!", "'.$Self->{LayoutObject}->{LanguageObject}->GetRecommendedCharset()),
         );
     }
     return $Output;
