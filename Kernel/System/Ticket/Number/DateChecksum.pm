00da499e59c49ca0b1e5b5602c8252267dd0bc98
martin <martin>
2004-10-12 14:01:00 +0000

    replacec COUNTER with DATA (because of win32 systems)

--- Kernel/System/Ticket/Number/DateChecksum.pm
+++ Kernel/System/Ticket/Number/DateChecksum.pm
@@ -1,173 +1,156 @@
 # --
 # Ticket/Number/DateChecksum.pm - a date ticket number generator
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2004 Martin Edenhofer <martin+code@otrs.org>
+# Copyright (C) 2002 Stefan Schmidt <jsj@jsj.dyndns.org>
 # --
-# $Id: DateChecksum.pm,v 1.35 2009/05/15 10:12:02 martin Exp $
+# $Id: DateChecksum.pm,v 1.9.6.1 2004/10/12 14:01:00 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 # --
-
 # Note:
 # available objects are: ConfigObject, LogObject and DBObject
-#
+# --
 # The algorithm to calculate the checksum is derived from the one
 # Deutsche Bundesbahn (german railway company) uses for calculation
 # of the check digit of their vehikel numbering.
 # The checksum is calculated by alternately multiplying the digits
-# with 1 and 2 and adding the resulsts from left to right of the
+# with 1 and 2 and adding the resulsts from left to right of the 
 # vehikel number. The modulus to 10 of this sum is substracted from
 # 10. See: http://www.pruefziffernberechnung.de/F/Fahrzeugnummer.shtml
 # (german)
-#
+# --
 # Generates ticket numbers like yyyymmddssID#####C (e. g. 2002062310100011)
+# --
 
 package Kernel::System::Ticket::Number::DateChecksum;
 
 use strict;
-use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.35 $) [1];
+$VERSION = '$Revision: 1.9.6.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
-sub TicketCreateNumber {
-    my ( $Self, $JumpCounter ) = @_;
-    if ( !$JumpCounter ) {
-        $JumpCounter = 0;
-    }
-
-    # get needed config options
-    my $CounterLog = $Self->{ConfigObject}->Get('Ticket::CounterLog');
-    my $SystemID   = $Self->{ConfigObject}->Get('SystemID');
-
+sub CreateTicketNr {
+    my $Self = shift;
+    my $JumpCounter = shift || 0;
+    # get needed config options 
+    my $CounterLog = $Self->{ConfigObject}->Get('CounterLog');
+    my $SystemID = $Self->{ConfigObject}->Get('SystemID');
     # get current time
-    my ( $Sec, $Min, $Hour, $Day, $Month, $Year ) = $Self->{TimeObject}->SystemTime2Date(
-        SystemTime => $Self->{TimeObject}->SystemTime(),
-    );
-
+    my ($Sec, $Min, $Hour, $Day, $Month, $Year) = localtime(time);
+    $Year = $Year+1900;
+    $Month = $Month+1;
+    $Month  = "0$Month" if ($Month <10);
+    $Day = "0$Day" if ($Day <10);
     # read count
-    my $Count      = 0;
+    my $Count = 0;
     my $LastModify = '';
-    if ( -f $CounterLog ) {
-        my $ContentSCALARRef = $Self->{MainObject}->FileRead(
-            Location => $CounterLog,
-        );
-        if ( $ContentSCALARRef && ${$ContentSCALARRef} ) {
-            ( $Count, $LastModify ) = split( /;/, ${$ContentSCALARRef} );
-
-            # just debug
-            if ( $Self->{Debug} > 0 ) {
-                $Self->{LogObject}->Log(
-                    Priority => 'debug',
-                    Message  => "Read counter from $CounterLog: $Count",
-                );
-            }
+    if (-f $CounterLog) {
+        open (DATA, "< $CounterLog") || die "Can't open $CounterLog: $!";
+        my $Line = <DATA>;
+        ($Count, $LastModify) = split(/;/, $Line);
+        close (DATA);
+        # just debug
+        if ($Self->{Debug} > 0) {
+            $Self->{LogObject}->Log(
+              Priority => 'debug',
+              Message => "Read counter from $CounterLog: $Count",
+            );
         }
     }
-
     # check if we need to reset the counter
-    if ( !$LastModify || $LastModify ne "$Year-$Month-$Day" ) {
+    if (!$LastModify || $LastModify ne "$Year-$Month-$Day") {
         $Count = 0;
     }
-
     # count auto increment ($Count++)
     $Count++;
     $Count = $Count + $JumpCounter;
-    my $Content = $Count . ";$Year-$Month-$Day;";
-
     # write new count
-    my $Write = $Self->{MainObject}->FileWrite(
-        Location => $CounterLog,
-        Content  => \$Content,
-    );
-    if ($Write) {
-        if ( $Self->{Debug} > 0 ) {
+    if (open (DATA, "> $CounterLog")) {
+        flock (DATA, 2) || warn "Can't set file lock ($CounterLog): $!";
+        print DATA $Count.";$Year-$Month-$Day;\n";
+        close (DATA);
+        # just debug
+        if ($Self->{Debug} > 0) {
             $Self->{LogObject}->Log(
-                Priority => 'debug',
-                Message  => "Write counter: $Count",
+              Priority => 'debug',
+              Message => "Write counter: $Count",
             );
         }
     }
-
+    else {
+        # just debug
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message => "Can't write $CounterLog: $!",
+        );
+        die "Can't write $CounterLog: $!";
+    }
     # pad ticket number with leading '0' to length 5
-    while ( length($Count) < 5 ) {
-        $Count = '0' . $Count;
+    while ( length( $Count ) < 5 ) {
+        $Count = "0" . $Count;
     }
-
     # create new ticket number
-    my $Tn = $Year . $Month . $Day . $SystemID . $Count;
-
+    my $Tn = $Year.$Month.$Day.$SystemID.$Count;
     # calculate a checksum
-    my $ChkSum = 0;
-    my $Mult   = 1;
-    for ( my $i = 0; $i < length($Tn); ++$i ) {
+    my $chksum = 0;
+    my $mult = 1;
+    for ( my $i = 0; $i<length($Tn); ++$i ) {
         my $Digit = substr( $Tn, $i, 1 );
-        $ChkSum = $ChkSum + ( $Mult * $Digit );
-        $Mult += 1;
-        if ( $Mult == 3 ) {
-            $Mult = 1;
+        $chksum = $chksum + ( $mult * $Digit );
+        $mult += 1;
+        if ( $mult == 3 ) {
+          $mult = 1;
         }
     }
-    $ChkSum %= 10;
-    $ChkSum = 10 - $ChkSum;
-    if ( $ChkSum == 10 ) {
-        $ChkSum = 1;
+    $chksum %= 10;
+    $chksum = 10 - $chksum;
+    if ($chksum == 10) {
+        $chksum = 1;
     }
-
     # add checksum to ticket number
-    $Tn = $Tn . $ChkSum;
-
-    # Check ticket number. If exists generate new one!
-    if ( $Self->TicketCheckNumber( Tn => $Tn ) ) {
+    $Tn = $Tn.$chksum;
+    # Check ticket number. If exists generate new one! 
+    if ($Self->CheckTicketNr(Tn=>$Tn)) {
         $Self->{LoopProtectionCounter}++;
-        if ( $Self->{LoopProtectionCounter} >= 16000 ) {
-
-            # loop protection
-            $Self->{LogObject}->Log(
-                Priority => 'error',
-                Message  => "CounterLoopProtection is now $Self->{LoopProtectionCounter}!"
-                    . " Stopped TicketCreateNumber()!",
-            );
-            return;
+        if ($Self->{LoopProtectionCounter} >= 1000) {
+          # loop protection
+          $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message => "CounterLoopProtection is now $Self->{LoopProtectionCounter}!".
+                   " Stoped CreateTicketNr()!",
+          );
+          return;
         }
-
         # create new ticket number again
         $Self->{LogObject}->Log(
-            Priority => 'notice',
-            Message  => "Tn ($Tn) exists! Creating a new one.",
+          Priority => 'notice',
+          Message => "Tn ($Tn) exists! Creating new one.",
         );
-        $Tn = $Self->TicketCreateNumber( $Self->{LoopProtectionCounter} );
+        $Tn = $Self->CreateTicketNr($Self->{LoopProtectionCounter});
     }
     return $Tn;
 }
-
+# --
 sub GetTNByString {
-    my ( $Self, $String ) = @_;
-    if ( !$String ) {
-        return;
-    }
-
-    # get needed config options
-    my $CheckSystemID = $Self->{ConfigObject}->Get('Ticket::NumberGenerator::CheckSystemID');
-    my $SystemID      = '';
-    if ($CheckSystemID) {
-        $SystemID = $Self->{ConfigObject}->Get('SystemID');
-    }
-    my $TicketHook        = $Self->{ConfigObject}->Get('Ticket::Hook');
-    my $TicketHookDivider = $Self->{ConfigObject}->Get('Ticket::HookDivider');
-
-    # check current setting
-    if ( $String =~ /\Q$TicketHook$TicketHookDivider\E(\d{4,10}$SystemID\d{4,40})/i ) {
+    my $Self = shift;
+    my $String = shift || return;
+    # get needed config options 
+    my $TicketHook = $Self->{ConfigObject}->Get('TicketHook');
+    # check ticket number
+    if ($String =~ /$TicketHook:+.{0,1}(\d{8,40})\-FW/i) {
         return $1;
     }
-
-    # check default setting
-    if ( $String =~ /\Q$TicketHook\E:\s{0,2}(\d{4,10}$SystemID\d{4,40})/i ) {
-        return $1;
+    else {
+        if ($String =~ /$TicketHook:+.{0,1}(\d{8,40})/i) {
+            return $1;
+        }
+        else {
+            return;
+        }
     }
-
-    return;
 }
-
+# --
 1;
