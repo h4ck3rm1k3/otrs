26945a3c11daf4259f112409b373505298baacd1
martin <martin>
2005-07-01 06:12:51 +0000

    fixed bug 810 - single quote in comment field generates errors

--- Kernel/System/User/Preferences/DB.pm
+++ Kernel/System/User/Preferences/DB.pm
@@ -1,125 +1,123 @@
 # --
 # Kernel/System/User/Preferences/DB.pm - some user functions
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2005 Martin Edenhofer <martin+code@otrs.org>
 # --
-# $Id: DB.pm,v 1.19 2009/10/07 17:34:09 martin Exp $
+# $Id: DB.pm,v 1.6.6.1 2005/07/01 06:12:51 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
 # --
 
 package Kernel::System::User::Preferences::DB;
 
 use strict;
-use warnings;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.19 $) [1];
+$VERSION = '$Revision: 1.6.6.1 $';
+$VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
+# --
 sub new {
-    my ( $Type, %Param ) = @_;
+    my $Type = shift;
+    my %Param = @_;
 
     # allocate new hash for object
     my $Self = {};
-    bless( $Self, $Type );
+    bless ($Self, $Type);
 
     # check needed objects
-    for (qw(DBObject ConfigObject LogObject)) {
+    foreach (qw(DBObject ConfigObject LogObject)) {
         $Self->{$_} = $Param{$_} || die "Got no $_!";
     }
 
     # preferences table data
     $Self->{PreferencesTable} = $Self->{ConfigObject}->Get('PreferencesTable')
-        || 'user_preferences';
+      || 'user_preferences';
     $Self->{PreferencesTableKey} = $Self->{ConfigObject}->Get('PreferencesTableKey')
-        || 'preferences_key';
+      || 'preferences_key';
     $Self->{PreferencesTableValue} = $Self->{ConfigObject}->Get('PreferencesTableValue')
-        || 'preferences_value';
+      || 'preferences_value';
     $Self->{PreferencesTableUserID} = $Self->{ConfigObject}->Get('PreferencesTableUserID')
-        || 'user_id';
+      || 'user_id';
 
     return $Self;
 }
-
+# --
 sub SetPreferences {
-    my ( $Self, %Param ) = @_;
-
+    my $Self = shift;
+    my %Param = @_;
     # check needed stuff
-    for (qw(UserID Key)) {
-        if ( !$Param{$_} ) {
-            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
-            return;
-        }
+    foreach (qw(UserID Key)) {
+      if (!$Param{$_}) {
+        $Self->{LogObject}->Log(Priority => 'error', Message => "Need $_!");
+        return;
+      }
     }
-
+    # db quote
+    foreach (keys %Param) {
+        $Param{$_} = $Self->{DBObject}->Quote($Param{$_});
+    }
     # delete old data
-    return if !$Self->{DBObject}->Do(
-        SQL => "DELETE FROM $Self->{PreferencesTable} WHERE "
-            . " $Self->{PreferencesTableUserID} = ? AND $Self->{PreferencesTableKey} = ?",
-        Bind => [ \$Param{UserID}, \$Param{Key} ],
-    );
+    if (!$Self->{DBObject}->Do(
+       SQL => "DELETE FROM $Self->{PreferencesTable} ".
+              " WHERE ".
+              " $Self->{PreferencesTableUserID} = $Param{UserID} ".
+              " AND ".
+              " $Self->{PreferencesTableKey} = '$Param{Key}'",
+    )) {
+        $Self->{LogObject}->Log(
+          Priority => 'error',
+          Message => "Can't delete $Self->{PreferencesTable}!",
+        );
+        return;
+    }
 
     # insert new data
-    return if !$Self->{DBObject}->Do(
-        SQL => "INSERT INTO $Self->{PreferencesTable} ($Self->{PreferencesTableUserID}, "
-            . " $Self->{PreferencesTableKey}, $Self->{PreferencesTableValue}) "
-            . " VALUES (?, ?, ?)",
-        Bind => [ \$Param{UserID}, \$Param{Key}, \$Param{Value} ],
-    );
+    if (!$Self->{DBObject}->Do(
+       SQL => "INSERT INTO $Self->{PreferencesTable} ($Self->{PreferencesTableUserID}, ".
+              " $Self->{PreferencesTableKey}, $Self->{PreferencesTableValue}) " .
+              " VALUES ($Param{UserID}, '$Param{Key}', '$Param{Value}')",
+    )) {
+        $Self->{LogObject}->Log(
+          Priority => 'error',
+          Message => "Can't insert new $Self->{PreferencesTable}!",
+        );
+        return;
+    }
+
     return 1;
 }
-
+# --
 sub GetPreferences {
-    my ( $Self, %Param ) = @_;
-
-    # check needed stuff
-    for (qw(UserID)) {
-        if ( !$Param{$_} ) {
-            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
-            return;
-        }
-    }
-
-    # get preferences
-    return if !$Self->{DBObject}->Prepare(
-        SQL => "SELECT $Self->{PreferencesTableKey}, $Self->{PreferencesTableValue} "
-            . " FROM $Self->{PreferencesTable} WHERE $Self->{PreferencesTableUserID} = ?",
-        Bind => [ \$Param{UserID} ],
-    );
+    my $Self = shift;
+    my %Param = @_;
     my %Data;
-    while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
-        $Data{ $Row[0] } = $Row[1];
+    # check needed stuff
+    foreach (qw(UserID)) {
+      if (!$Param{$_}) {
+        $Self->{LogObject}->Log(Priority => 'error', Message => "Need $_!");
+        return;
+      }
     }
+    # db quote
+    foreach (keys %Param) {
+        $Param{$_} = $Self->{DBObject}->Quote($Param{$_});
+    }
+    # get preferences
+    my $SQL = "SELECT $Self->{PreferencesTableKey}, $Self->{PreferencesTableValue} " .
+        " FROM " .
+        " $Self->{PreferencesTable} ".
+        " WHERE " .
+        " $Self->{PreferencesTableUserID} = $Param{UserID}";
 
+    $Self->{DBObject}->Prepare(SQL => $SQL);
+    while (my @RowTmp = $Self->{DBObject}->FetchrowArray()) {
+        $Data{$RowTmp[0]} = $RowTmp[1];
+    }
     # return data
     return %Data;
 }
-
-sub SearchPreferences {
-    my ( $Self, %Param ) = @_;
-
-    my %UserID;
-    my $Key   = $Param{Key}   || '';
-    my $Value = $Param{Value} || '';
-
-    # get preferences
-    my $SQL = "SELECT $Self->{PreferencesTableUserID}, $Self->{PreferencesTableValue} "
-        . " FROM "
-        . " $Self->{PreferencesTable} "
-        . " WHERE "
-        . " $Self->{PreferencesTableKey} = '"
-        . $Self->{DBObject}->Quote($Key) . "'" . " AND "
-        . " LOWER($Self->{PreferencesTableValue}) LIKE LOWER('"
-        . $Self->{DBObject}->Quote( $Value, 'Like' ) . "')";
-
-    return if !$Self->{DBObject}->Prepare( SQL => $SQL );
-    while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
-        $UserID{ $Row[0] } = $Row[1];
-    }
-
-    # return data
-    return %UserID;
-}
+# --
 
 1;
