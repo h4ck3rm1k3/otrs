8b9f514af0ca7c1abdc3ad743c6afb9814baacac
mh <mh>
2006-12-14 19:30:39 +0000

    update to version 0.65_3

--- Kernel/cpan-lib/XML/Parser/Lite.pm
+++ Kernel/cpan-lib/XML/Parser/Lite.pm
@@ -4,7 +4,7 @@
 # SOAP::Lite is free software; you can redistribute it
 # and/or modify it under the same terms as Perl itself.
 #
-# $oId: Lite.pm,v 1.1.1.1 2002/11/01 14:53:57 paulclinger Exp $
+# $Id: Lite.pm,v 1.2 2005/02/22 01:47:42 byrnereese Exp $
 #
 # ======================================================================
 
@@ -12,7 +12,8 @@ package XML::Parser::Lite;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = sprintf("%d.%s", map {s/_//g; $_} q$oName: release-0_60-public $ =~ /-(\d+)_([\d_]+)/);
+#$VERSION = sprintf("%d.%s", map {s/_//g; $_} q$Name:  $ =~ /-(\d+)_([\d_]+)/);
+$VERSION = '0.65_3';
 
 sub new { 
   my $self = shift;
