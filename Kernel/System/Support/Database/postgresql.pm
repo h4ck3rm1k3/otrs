2d293f35deda7b2da3431e92647c4fdc8a157c7f
martin <martin>
2007-08-29 16:31:50 +0000

    Added some postgrsql checks.

--- Kernel/System/Support/Database/postgresql.pm
+++ Kernel/System/Support/Database/postgresql.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Support/Database/postgresql.pm - all required system informations
 # Copyright (C) 2001-2007 OTRS GmbH, http://otrs.org/
 # --
-# $Id: postgresql.pm,v 1.1 2007/06/18 19:56:07 martin Exp $
+# $Id: postgresql.pm,v 1.2 2007/08/29 16:31:50 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -15,7 +15,7 @@ use strict;
 use Kernel::System::XML;
 
 use vars qw(@ISA $VERSION);
-$VERSION = '$Revision: 1.1 $';
+$VERSION = '$Revision: 1.2 $';
 $VERSION =~ s/^\$.*:\W(.*)\W.+?$/$1/;
 
 sub new {
@@ -80,7 +80,110 @@ sub AdminChecksGet {
             return;
         }
     }
-
+    # verion check
+    my $Check = 'Failed';
+    my $Message = 'No version found!';
+    $Self->{DBObject}->Prepare(SQL => "show all");
+    while (my @Row = $Self->{DBObject}->FetchrowArray()) {
+            if ($Row[0] =~ /^server_version/i) {
+                if ($Row[1] =~ /^(\d{1,3}).*$/) {
+                    if ($1 > 7) {
+                        $Check = 'OK';
+                        $Message = "$Row[1]";
+                    }
+                    else {
+                        $Check = 'Failed';
+                        $Message = "Its version $Row[1], you should use 8.x or higner.";
+                    }
+                }
+                else {
+                    $Check = 'Failed';
+                    $Message = "Unkown version $Row[1]";
+                }
+            }
+    }
+    push (@DataArray,
+        {
+            Key => 'version',
+            Name => 'version',
+            Description => "Check version.",
+            Comment => $Message,
+            Check => $Check,
+        },
+    );
+    # utf-8 client check
+    if ($Self->{ConfigObject}->Get('DefaultCharset') =~ /utf(\-8|8)/i) {
+        my $Check = 'Failed';
+        my $Message = 'No client_encoding found!';
+        $Self->{DBObject}->Prepare(SQL => "show all");
+        while (my @Row = $Self->{DBObject}->FetchrowArray()) {
+            if ($Row[0] =~ /^client_encoding/i) {
+                $Message = "client_encoding found but it's set to $Row[1] (need to be UNICODE)";
+                if ($Row[1] =~ /UNICODE/i) {
+                    $Check = 'OK';
+                    $Message = "$Row[1]";
+                }
+            }
+        }
+        push (@DataArray,
+            {
+                Key => 'utf8 client connection',
+                Name => 'utf8 client connection',
+                Description => "Check the utf8 client connection.",
+                Comment => $Message,
+                Check => $Check,
+            },
+        );
+    }
+    # utf-8 server check
+    if ($Self->{ConfigObject}->Get('DefaultCharset') =~ /utf(\-8|8)/i) {
+        my $Check = 'Failed';
+        my $Message = 'No server_encoding found!';
+        $Self->{DBObject}->Prepare(SQL => "show all");
+        while (my @Row = $Self->{DBObject}->FetchrowArray()) {
+            if ($Row[0] =~ /^server_encoding/i) {
+                $Message = "server_encoding found but it's set to $Row[1] (need to be UNICODE)";
+                if ($Row[1] =~ /UNICODE/i) {
+                    $Check = 'OK';
+                    $Message = "$Row[1]";
+                }
+            }
+        }
+        push (@DataArray,
+            {
+                Key => 'utf8 server connection',
+                Name => 'utf8 server connection',
+                Description => "Check the utf8 server connection.",
+                Comment => $Message,
+                Check => $Check,
+            },
+        );
+    }
+    # verion check
+    $Check = 'Failed';
+    $Message = 'No DateStyle found!';
+    $Self->{DBObject}->Prepare(SQL => "show all");
+    while (my @Row = $Self->{DBObject}->FetchrowArray()) {
+            if ($Row[0] =~ /^DateStyle/i) {
+                if ($Row[1] =~ /^ISO/i) {
+                    $Check = 'OK';
+                    $Message = "$Row[1]";
+                }
+                else {
+                    $Check = 'Failed';
+                    $Message = "Unkown DateStyle ($Row[1]) need ISO.";
+                }
+            }
+    }
+    push (@DataArray,
+        {
+            Key => 'DateStyle',
+            Name => 'DateStyle',
+            Description => "Check DateStyle.",
+            Comment => $Message,
+            Check => $Check,
+        },
+    );
     # table check
     my $File = $Self->{ConfigObject}->Get('Home')."/scripts/database/otrs-schema.xml";
     if (-f $File) {
