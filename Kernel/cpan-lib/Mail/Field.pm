eca694b3c11f21b1f4bdcae624410c2acdee35f6
mh <mh>
2007-10-24 15:49:10 +0000

    Update to version 1.77.

--- Kernel/cpan-lib/Mail/Field.pm
+++ Kernel/cpan-lib/Mail/Field.pm
@@ -13,7 +13,7 @@ use Carp;
 use strict;
 use vars qw($AUTOLOAD $VERSION);
 
-$VERSION = "1.74";
+$VERSION = "1.77";
 
 unless(defined &UNIVERSAL::can) {
     *UNIVERSAL::can = sub {
--- Kernel/cpan-lib/Mail/Send.pm
+++ Kernel/cpan-lib/Mail/Send.pm
@@ -8,7 +8,7 @@ use Carp;
 use vars qw($VERSION);
 require Mail::Mailer;
 
-$VERSION = "1.74";
+$VERSION = "1.77";
 
 sub Version { $VERSION }
 
