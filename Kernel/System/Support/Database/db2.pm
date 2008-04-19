6573aac7787f66340c5c1dcd95b9d9f09794c800
martin <martin>
2008-04-19 20:51:50 +0000

    Added db2 checks.

---
+++ Kernel/System/Support/Database/db2.pm
@@ -0,0 +1,185 @@
+# --
+# Kernel/System/Support/Database/db2.pm - all required system informations
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
+# --
+# $Id: db2.pm,v 1.1 2008/04/19 20:51:50 martin Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+
+package Kernel::System::Support::Database::db2;
+
+use strict;
+use warnings;
+
+use Kernel::System::XML;
+
+use vars qw(@ISA $VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for (qw(ConfigObject LogObject DBObject)) {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+
+    $Self->{XMLObject} = Kernel::System::XML->new(%Param);
+
+    return $Self;
+}
+
+sub SupportConfigArrayGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw()) {
+        if ( !$Param{$_} ) {
+            $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
+            return;
+        }
+    }
+}
+
+sub SupportInfoGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Param{ModuleInputHash} ) {
+        $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
+        return;
+    }
+    if ( ref( $Param{ModuleInputHash} ) ne 'HASH' ) {
+        $Self->{LogObject}->Log(
+            Priority => 'error',
+            Message  => 'ModuleInputHash must be a hash reference!',
+        );
+        return;
+    }
+
+    # add new function name here
+    my @ModuleList = (
+        '_TableCheck',
+    );
+
+    my @DataArray;
+
+    FUNCTIONNAME:
+    for my $FunctionName (@ModuleList) {
+
+        # run function and get check data
+        my $Check = $Self->$FunctionName( Type => $Param{ModuleInputHash}->{Type} || '', );
+
+        next FUNCTIONNAME if !$Check;
+
+        # attach check data if valid
+        push @DataArray, $Check;
+    }
+
+    return \@DataArray;
+}
+
+sub AdminChecksGet {
+    my ( $Self, %Param ) = @_;
+
+    # add new function name here
+    my @ModuleList = (
+        '_TableCheck',
+    );
+
+    my @DataArray;
+
+    FUNCTIONNAME:
+    for my $FunctionName (@ModuleList) {
+
+        # run function and get check data
+        my $Check = $Self->$FunctionName();
+
+        next FUNCTIONNAME if !$Check;
+
+        # attach check data if valid
+        push @DataArray, $Check;
+    }
+
+    return \@DataArray;
+}
+
+sub _TableCheck {
+    my ( $Self, %Param ) = @_;
+
+    my $Data = {};
+
+    # table check
+    my $File = $Self->{ConfigObject}->Get('Home') . '/scripts/database/otrs-schema.xml';
+    if ( -f $File ) {
+        my $Count   = 0;
+        my $Check   = 'Failed';
+        my $Message = '';
+        my $Content = '';
+        my $In;
+        if ( open( $In, '<', $File) ) {
+            while (<$In>) {
+                $Content .= $_;
+            }
+            close($In);
+            my @XMLHash = $Self->{XMLObject}->XMLParse2XMLHash( String => $Content );
+            for my $Table ( @{ $XMLHash[1]->{database}->[1]->{Table} } ) {
+                if ($Table) {
+                    if ($Table->{Name} eq 'system_user') {
+                        $Table->{Name} = 'system_user2';
+                    }
+                    $Count++;
+                    if ( $Self->{DBObject}->Prepare( SQL => "select * from $Table->{Name}", Limit => 1 ) )
+                    {
+                        while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
+                        }
+                    }
+                    else {
+                        $Message .= "$Table->{Name}, ";
+                    }
+                }
+            }
+            if ($Message) {
+                $Message = "Table dosn't exists: $Message";
+            }
+            else {
+                $Check   = 'OK';
+                $Message = "$Count Tables";
+            }
+            $Data = {
+                Key         => 'Table',
+                Name        => 'Table',
+                Description => 'Check existing framework tables.',
+                Comment     => $Message,
+                Check       => $Check,
+            };
+        }
+        else {
+            $Data = {
+                Key         => 'Table',
+                Name        => 'Table',
+                Description => 'Check existing framework tables.',
+                Comment     => "Can't open file $File: $!",
+                Check       => $Check,
+            };
+        }
+    }
+    else {
+        $Data = {
+            Key         => 'Table',
+            Name        => 'Table',
+            Description => 'Check existing framework tables.',
+            Comment     => "Can't find file $File!",
+            Check       => 'Failed',
+        };
+    }
+    return $Data;
+}
+1;
