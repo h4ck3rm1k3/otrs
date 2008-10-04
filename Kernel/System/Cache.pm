01cf1466e626f3b4110aa30d25a3130f0d9662ab
martin <martin>
2008-10-04 15:04:27 +0000

    Added option to clean up only one cache type.

--- Kernel/System/Cache.pm
+++ Kernel/System/Cache.pm
@@ -1,12 +1,12 @@
 # --
 # Kernel/System/Cache.pm - all cache functions
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2008 OTRS AG, http://otrs.org/
 # --
-# $Id: Cache.pm,v 1.20 2010/05/06 18:33:12 mb Exp $
+# $Id: Cache.pm,v 1.11.2.1 2008/10/04 15:04:27 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 # --
 
 package Kernel::System::Cache;
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.20 $) [1];
+$VERSION = qw($Revision: 1.11.2.1 $) [1];
 
 =head1 NAME
 
@@ -36,29 +36,22 @@ All cache functions.
 create an object
 
     use Kernel::Config;
-    use Kernel::System::Encode;
     use Kernel::System::Log;
     use Kernel::System::Main;
     use Kernel::System::Cache;
 
     my $ConfigObject = Kernel::Config->new();
-    my $EncodeObject = Kernel::System::Encode->new(
+    my $LogObject    = Kernel::System::Log->new(
         ConfigObject => $ConfigObject,
-    );
-    my $LogObject = Kernel::System::Log->new(
-        ConfigObject => $ConfigObject,
-        EncodeObject => $EncodeObject,
     );
     my $MainObject = Kernel::System::Main->new(
         ConfigObject => $ConfigObject,
-        EncodeObject => $EncodeObject,
         LogObject    => $LogObject,
     );
     my $CacheObject = Kernel::System::Cache->new(
         ConfigObject => $ConfigObject,
         LogObject    => $LogObject,
         MainObject   => $MainObject,
-        EncodeObject => $EncodeObject,
     );
 
 =cut
@@ -74,15 +67,13 @@ sub new {
     $Self->{Debug} = $Param{Debug} || 0;
 
     # check needed objects
-    for (qw(MainObject ConfigObject LogObject EncodeObject)) {
+    for (qw(MainObject ConfigObject LogObject)) {
         $Self->{$_} = $Param{$_} || die "Got no $_!";
     }
 
     # cache backend
     my $CacheModule = $Self->{ConfigObject}->Get('Cache::Module')
-        || 'Kernel::System::Cache::FileStorable';
-
-    #        || 'Kernel::System::Cache::Memcached';
+        || 'Kernel::System::Cache::File';
     if ( !$Self->{MainObject}->Require($CacheModule) ) {
         die "Can't load backend module $CacheModule! $@";
     }
@@ -109,7 +100,7 @@ sub Set {
     my ( $Self, %Param ) = @_;
 
     # check needed stuff
-    for (qw(Type Key Value TTL)) {
+    for (qw(Type Key Value)) {
         if ( !defined $Param{$_} ) {
             $Self->{LogObject}->Log( Priority => 'error', Message => "Need $_!" );
             return;
@@ -201,25 +192,15 @@ sub Delete {
 
 =item CleanUp()
 
-delete chache or parts of the cache
-
-    To delete the whole cache
+delete all caches
 
     $CacheObject->CleanUp();
 
     of if you want to cleanup only one object cache
 
     $CacheObject->CleanUp(
-        Type => 'ObjectName', # only A-z chars are usable
+        Type => 'ObjectName', # only A-z chars usable
     );
-
-    of if you want to cleanup only one object cache with expired caches
-
-    $CacheObject->CleanUp(
-        Type    => 'ObjectName', # only A-z chars are usable
-        Expired => 1, # 1|0, default 0
-    );
-
 =cut
 
 sub CleanUp {
@@ -241,16 +222,16 @@ sub CleanUp {
 
 =head1 TERMS AND CONDITIONS
 
-This software is part of the OTRS project (L<http://otrs.org/>).
+This Software is part of the OTRS project (http://otrs.org/).
 
 This software comes with ABSOLUTELY NO WARRANTY. For details, see
-the enclosed file COPYING for license information (AGPL). If you
-did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
 
 =cut
 
 =head1 VERSION
 
-$Revision: 1.20 $ $Date: 2010/05/06 18:33:12 $
+$Revision: 1.11.2.1 $ $Date: 2008/10/04 15:04:27 $
 
 =cut
