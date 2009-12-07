c59a5429dd6b3c36203aa0912686f0f53f8f1173
mg <mg>
2009-12-07 14:57:08 +0000

    - (2009/12/07) Fixed bug#4597 - Unnecessary slash in download links.  - (2009/12/07) Fixed bug#2768 - AttachmentsContent-Disposition header downloading a non-ASCII filename.

--- Kernel/Output/HTML/ArticleAttachmentDownload.pm
+++ Kernel/Output/HTML/ArticleAttachmentDownload.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Output/HTML/ArticleAttachmentDownload.pm
-# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: ArticleAttachmentDownload.pm,v 1.15 2011/03/10 00:16:44 mp Exp $
+# $Id: ArticleAttachmentDownload.pm,v 1.10.2.1 2009/12/07 14:57:08 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.15 $) [1];
+$VERSION = qw($Revision: 1.10.2.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
