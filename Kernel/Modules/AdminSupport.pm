0cf7908fb93fce4f778368e8dc2a7faef2e16f6e
martin <martin>
2010-01-12 10:51:28 +0000

    OptionStrgHashRef() is not available in HEAD, use BuildSelection instant.

--- Kernel/Modules/AdminSupport.pm
+++ Kernel/Modules/AdminSupport.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AdminSupport.pm - show support information
-# Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AdminSupport.pm,v 1.22 2009/12/01 15:06:47 martin Exp $
+# $Id: AdminSupport.pm,v 1.23 2010/01/12 10:51:28 martin Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::Support;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.22 $) [1];
+$VERSION = qw($Revision: 1.23 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -301,7 +301,7 @@ sub Run {
 
         # create data hash reference
         my $DataHash = $Self->{SupportObject}->AdminChecksGet();
-        $Param{ModeStrg} = $Self->{LayoutObject}->OptionStrgHashRef(
+        $Param{ModeStrg} = $Self->{LayoutObject}->BuildSelection(
             Data => {
                 1 => '1 * Normal (ca. 25 sec)',
                 3 => '3 * High   (ca. 75 sec)',
