ea551dea431c91da51f14501d1553d27a015b284
mn <mn>
2010-06-21 09:26:55 +0000

    Fixed bug # 5082: Search templates are not launched when clicked on.

---
+++ Kernel/Output/HTML/NavBarTicketSearchProfile.pm
@@ -0,0 +1,75 @@
+# --
+# Kernel/Output/HTML/NavBarTicketSearchProfile.pm
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# --
+# $Id: NavBarTicketSearchProfile.pm,v 1.3.2.1 2010/06/21 09:26:55 mn Exp $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::NavBarTicketSearchProfile;
+
+use strict;
+use warnings;
+
+use Kernel::System::SearchProfile;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.3.2.1 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for (qw(ConfigObject LogObject DBObject TicketObject LayoutObject UserID UserObject)) {
+        $Self->{$_} = $Param{$_} || die "Got no $_!";
+    }
+
+    $Self->{SearchProfileObject} = Kernel::System::SearchProfile->new(%Param);
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # get user data
+    my %User = $Self->{UserObject}->GetUserData(
+        UserID => $Self->{UserID},
+        Cached => 1,
+    );
+
+    # create search profiles string
+    my $ProfilesStrg = $Self->{LayoutObject}->OptionStrgHashRef(
+        Data => {
+            '', '-',
+            $Self->{SearchProfileObject}->SearchProfileList(
+                Base      => 'TicketSearch',
+                UserLogin => $User{UserLogin},
+            ),
+        },
+        Name       => 'Profile',
+        SelectedID => '',
+        OnChange   => 'document.SearchProfile.submit()',
+        Max        => $Param{Config}->{MaxWidth},
+    );
+
+    my %Return = ();
+    $Return{'0990000'} = {
+        Block       => $Param{Config}->{Block},
+        Description => $Param{Config}->{Description},
+        Name        => $Param{Config}->{Name},
+        Image       => '',
+        Link        => $ProfilesStrg,
+        AccessKey   => '',
+    };
+    return %Return;
+}
+
+1;
