1a52ed03dbdd647e72320dd2dfdf0795a5e9ccd3
mg <mg>
2010-11-05 09:29:07 +0000

    Improved display and result error detection for perl package check.

--- Kernel/System/Support/OS.pm
+++ Kernel/System/Support/OS.pm
@@ -2,7 +2,7 @@
 # Kernel/System/Support/OS.pm - all required system information
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: OS.pm,v 1.20 2010/11/04 21:43:22 cg Exp $
+# $Id: OS.pm,v 1.21 2010/11/05 09:29:07 mg Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.20 $) [1];
+$VERSION = qw($Revision: 1.21 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -247,15 +247,20 @@ sub _PerlModulesCheck {
         }
         close($TmpSumString);
 
-        if ( $TmpLog =~ /Not\sinstalled!+/ ) {
+        if (
+            $TmpLog =~ m{Not \s installed! \s \(Required}smx
+            || $TmpLog =~ m{failed!}smx
+            )
+        {
             $ReturnHash = {
                 Name        => 'PerlModulesCheck',
                 Description => "Check Perl Modules installed.",
                 Comment =>
-                    "You have not installed one or more modules needed.",
-                Content    => $TmpLog,
+                    "There is an error in your installed perl modules configuration. Please contact your administrator."
+                ,
                 Check      => 'Failed',
                 BlockStyle => 'TextArea',
+                Content    => $TmpLog,
             };
 
         }
@@ -265,6 +270,8 @@ sub _PerlModulesCheck {
                 Description => "Check Perl Modules installed.",
                 Comment     => "All Perl modules needed are currently installed.",
                 Check       => 'OK',
+                BlockStyle  => 'TextArea',
+                Content     => $TmpLog,
             };
         }
     }
