91a32c853da9707823d39d6c61f642e127ea1a13
ub <ub>
2011-02-14 11:10:06 +0000

    Fixed bug# 6869 - ITSM 3 Link Object "close window" refreshes and settings lost.

--- Kernel/Modules/AgentLinkObject.pm
+++ Kernel/Modules/AgentLinkObject.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AgentLinkObject.pm - to link objects
-# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentLinkObject.pm,v 1.62 2012/01/06 13:00:04 mg Exp $
+# $Id: AgentLinkObject.pm,v 1.58.2.1 2011/02/14 11:10:06 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -17,7 +17,7 @@ use warnings;
 use Kernel::System::LinkObject;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.62 $) [1];
+$VERSION = qw($Revision: 1.58.2.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -114,9 +114,6 @@ sub Run {
         my $Output = $Self->{LayoutObject}->Header( Type => 'Small' );
 
         if ( $Self->{ParamObject}->GetParam( Param => 'SubmitDelete' ) ) {
-
-            # challenge token check for write action
-            $Self->{LayoutObject}->ChallengeTokenCheck();
 
             # delete all temporary links older than one day
             $Self->{LinkObject}->LinkCleanup(
@@ -233,9 +230,6 @@ sub Run {
 
         # add new links
         if ( $Self->{ParamObject}->GetParam( Param => 'SubmitLink' ) ) {
-
-            # challenge token check for write action
-            $Self->{LayoutObject}->ChallengeTokenCheck();
 
             # get the link target keys
             my @LinkTargetKeys = $Self->{ParamObject}->GetArray( Param => 'LinkTargetKeys' );
@@ -431,20 +425,12 @@ sub Run {
         }
 
         # start search
-        my $SearchList;
-        if (
-            %SearchParam
-            || $Self->{ConfigObject}->Get('Frontend::AgentLinkObject::WildcardSearch')
-            )
-        {
-
-            $SearchList = $Self->{LinkObject}->ObjectSearch(
-                Object       => $Form{TargetObject},
-                SubObject    => $Form{TargetSubObject},
-                SearchParams => \%SearchParam,
-                UserID       => $Self->{UserID},
-            );
-        }
+        my $SearchList = $Self->{LinkObject}->ObjectSearch(
+            Object       => $Form{TargetObject},
+            SubObject    => $Form{TargetSubObject},
+            SearchParams => \%SearchParam,
+            UserID       => $Self->{UserID},
+        );
 
         # remove the source object from the search list
         if ( $SearchList && $SearchList->{ $Form{SourceObject} } ) {
@@ -463,12 +449,15 @@ sub Run {
         }
 
         # get already linked objects
-        my $LinkListWithData = $Self->{LinkObject}->LinkListWithData(
-            Object => $Form{SourceObject},
-            Key    => $Form{SourceKey},
-            State  => $Form{State},
-            UserID => $Self->{UserID},
-        );
+        my $LinkListWithData = {};
+        if ( $SearchList && $SearchList->{ $Form{TargetObject} } ) {
+            $LinkListWithData = $Self->{LinkObject}->LinkListWithData(
+                Object => $Form{SourceObject},
+                Key    => $Form{SourceKey},
+                State  => $Form{State},
+                UserID => $Self->{UserID},
+            );
+        }
 
         if ( $LinkListWithData && $LinkListWithData->{ $Form{TargetObject} } ) {
 
