8ef5d77b4640aec909c044c4c479f600eee7189f
en <en>
2011-09-07 21:08:46 +0000

    Fixed bug#7591 - Email address field validation fails if name server does not respond.

--- Kernel/System/CheckItem.pm
+++ Kernel/System/CheckItem.pm
@@ -2,7 +2,7 @@
 # Kernel/System/CheckItem.pm - the global spelling module
 # Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: CheckItem.pm,v 1.41 2011/09/07 20:52:38 en Exp $
+# $Id: CheckItem.pm,v 1.40.2.1 2011/09/07 21:08:46 en Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.41 $) [1];
+$VERSION = qw($Revision: 1.40.2.1 $) [1];
 
 =head1 NAME
 
@@ -367,6 +367,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.41 $ $Date: 2011/09/07 20:52:38 $
+$Revision: 1.40.2.1 $ $Date: 2011/09/07 21:08:46 $
 
 =cut
