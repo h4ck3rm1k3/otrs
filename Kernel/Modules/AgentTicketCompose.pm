e21452abf44729762f680c76cb0813eebb6e9b45
ep <ep>
2012-02-29 14:36:19 +0000

    Fixed typos (DynamcField → DynamicField, all ready → already).

--- Kernel/Modules/AgentTicketCompose.pm
+++ Kernel/Modules/AgentTicketCompose.pm
@@ -2,7 +2,7 @@
 # Kernel/Modules/AgentTicketCompose.pm - to compose and send a message
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentTicketCompose.pm,v 1.159 2012/01/24 00:08:45 cr Exp $
+# $Id: AgentTicketCompose.pm,v 1.161 2012/02/29 14:36:19 ep Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -27,7 +27,7 @@ use Kernel::System::VariableCheck qw(:all);
 use Mail::Address;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.159 $) [1];
+$VERSION = qw($Revision: 1.161 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -387,12 +387,12 @@ sub Run {
     # convert dynamic field values into a structure for ACLs
     my %DynamicFieldACLParameters;
     DYNAMICFIELD:
-    for my $DynamcField ( keys %DynamicFieldValues ) {
-        next DYNAMICFIELD if !$DynamcField;
-        next DYNAMICFIELD if !$DynamicFieldValues{$DynamcField};
+    for my $DynamicField ( keys %DynamicFieldValues ) {
+        next DYNAMICFIELD if !$DynamicField;
+        next DYNAMICFIELD if !$DynamicFieldValues{$DynamicField};
 
-        $DynamicFieldACLParameters{ 'DynamicField_' . $DynamcField }
-            = $DynamicFieldValues{$DynamcField};
+        $DynamicFieldACLParameters{ 'DynamicField_' . $DynamicField }
+            = $DynamicFieldValues{$DynamicField};
     }
     $GetParam{DynamicField} = \%DynamicFieldACLParameters;
 
@@ -998,7 +998,7 @@ sub Run {
             my $To   = $Data{To};
             my $From = $Data{From};
 
-            # set OrigFrom for correct email quoteing (xxxx wrote)
+            # set OrigFrom for correct email quoting (xxxx wrote)
             $Data{OrigFrom} = $Data{From};
 
             # replace From/To, To/From because sender is agent
@@ -1008,7 +1008,7 @@ sub Run {
         }
         else {
 
-            # set OrigFrom for correct email quoteing (xxxx wrote)
+            # set OrigFrom for correct email quoting (xxxx wrote)
             $Data{OrigFrom} = $Data{From};
         }
 
